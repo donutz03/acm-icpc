b9efc1860c4d7181d2cab4723539e9a812256560
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2023-07-02 10:46:34 +0300

    Create Paralelogram2.cpp

---
+++ Paralelogram2.cpp
@@ -0,0 +1,25 @@
+include <cstdio>
+struct pt
+{
+    int x, y;
+};
+pt A[6];
+int tests;
+
+int main()
+{
+    freopen("paralelogram2.in", "r", stdin);
+    freopen("paralelogram2.out", "w", stdout);
+
+    scanf("%d", &tests);
+    while (tests--)
+    {
+        for (int i = 1; i <= 4; i++)
+            scanf("%d%d", &A[i].x, &A[i].y);
+        A[5] = A[1];
+        for (int i = 1; i <= 4; i++)
+            printf("%lf %lf ", ((double)A[i].x + A[i + 1].x) / 2, ((double)A[i].y + A[i + 1].y) / 2);
+        printf("\n");
+    }
+    return 0;
+}
